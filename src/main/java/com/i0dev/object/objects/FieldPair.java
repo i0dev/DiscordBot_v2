c50534441233bab7b702f4599a377b478c2877c8
i01 <67590686+i0dev@users.noreply.github.com>
2021-06-08 13:22:55 -0500

    Added a field pair option for the SQL manager.

---
+++ src/main/java/com/i0dev/object/objects/FieldPair.java
@@ -0,0 +1,14 @@
+package com.i0dev.object.objects;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FieldPair {
+    private String name;
+    private String query;
+}
