683c443f3f3b2e23568ed5f91baafb1d7d8e4faf
i01 <67590686+i0dev@users.noreply.github.com>
2021-06-08 13:25:06 -0500

    Changed the in game chat formatter to better suit the new map points handling

--- src/main/java/com/i0dev/modules/other/InGameChatFormatter.java
+++ src/main/java/com/i0dev/modules/other/InGameChatFormatter.java
@@ -1,6 +1,5 @@
 package com.i0dev.modules.other;
 
-import com.i0dev.modules.mapPoints.MapPointsManager;
 import com.i0dev.object.discordLinking.DPlayer;
 import com.i0dev.object.discordLinking.DPlayerEngine;
 import org.bukkit.entity.Player;
@@ -9,22 +8,17 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import java.util.List;
-
 public class InGameChatFormatter implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void asyncChatEvent(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String format = event.getFormat();
-        if (!format.contains("{discordbot_mapPoints_allegiance}")) return;
         DPlayer dPlayer = DPlayerEngine.getObjectFromIGN(player.getName());
 
-        List<String> servers = MapPointsManager.GENERAL_SERVER_LIST;
-        for (String server : servers) {
-            format = format.replace("{discordbot_mapPoints_" + server + "}",
-                    dPlayer == null ? "0" : dPlayer.getMapPointsMap().has(server) ? dPlayer.getMapPointsMap().get(server).getAsString() : "0");
-        }
+        format = format.replace("{discordbot_mapPoints}",
+                dPlayer == null ? "0" : dPlayer.getMapPoints() + "");
+
         event.setFormat(format);
     }
 
