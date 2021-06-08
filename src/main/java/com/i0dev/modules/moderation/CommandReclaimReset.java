eba452946a25b7a6f15954925e895b864ba20fd2
i01 <67590686+i0dev@users.noreply.github.com>
2021-06-08 13:17:41 -0500

    Added a command to reset all users reclaim status. Intended for begging of every map.

---
+++ src/main/java/com/i0dev/modules/moderation/CommandReclaimReset.java
@@ -0,0 +1,55 @@
+package com.i0dev.modules.moderation;
+
+import com.i0dev.modules.DiscordCommandManager;
+import com.i0dev.object.discordLinking.DPlayer;
+import com.i0dev.object.discordLinking.DPlayerEngine;
+import com.i0dev.object.engines.PermissionHandler;
+import com.i0dev.object.objects.DiscordCommand;
+import com.i0dev.utility.*;
+import com.i0dev.utility.util.MessageUtil;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class CommandReclaimReset extends DiscordCommand {
+
+    public static boolean REQUIRE_PERMISSIONS;
+    public static boolean REQUIRE_LITE_PERMISSIONS;
+    public static String MESSAGE_CONTENT;
+    public static String MESSAGE_FORMAT;
+    public static boolean COMMAND_ENABLED;
+
+    @Override
+    public void init() {
+        REQUIRE_PERMISSIONS = Configuration.getBoolean("commands.reclaim_reset.requirePermission");
+        REQUIRE_LITE_PERMISSIONS = Configuration.getBoolean("commands.reclaim_reset.permissionLiteMode");
+        MESSAGE_CONTENT = Configuration.getString("commands.reclaim_reset.messageContent");
+        MESSAGE_FORMAT = Configuration.getString("commands.reclaim_reset.format");
+        COMMAND_ENABLED = Configuration.getBoolean("commands.reclaim_reset.enabled");
+    }
+
+    public static void run(GuildMessageReceivedEvent e) {
+        if (!GlobalCheck.checkBasic(e, COMMAND_ENABLED, new PermissionHandler(REQUIRE_LITE_PERMISSIONS, REQUIRE_PERMISSIONS, false), "Reclaim Reset")) {
+            return;
+        }
+        String[] message = e.getMessage().getContentRaw().split(" ");
+        if (message.length != 1) {
+            e.getChannel().sendMessage(EmbedFactory.createEmbed(Placeholders.convert(MESSAGE_FORMAT.replace("{command}", GlobalConfig.GENERAL_BOT_PREFIX + DiscordCommandManager.RECLAIM_RESET_ALIASES.get(0)), e.getAuthor())).build()).queue();
+            return;
+        }
+
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+        service.execute(() -> {
+            for (Object o : DPlayerEngine.getCache()) {
+                DPlayer dPlayer = ((DPlayer) o);
+                dPlayer.setClaimedReclaim(false);
+                dPlayer.save();
+            }
+        });
+        service.shutdown();
+
+        MessageUtil.sendMessage(e.getMessageIdLong(), MESSAGE_CONTENT, e.getAuthor());
+    }
+}
+
